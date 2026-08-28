113accf3db1d9f0cf28b4bd4e30ff7a4afbd6970
Макаров Степан <st.mkv03@gmail.com>
2026-08-28 11:22:43 +0300

    feat(application): added read scenarios

--- app/domain/exceptions.py
+++ app/domain/exceptions.py
@@ -1,5 +1,22 @@
 class DomainError(Exception):
     """Base exception for domain-layer errors."""
 
+
 class InvalidCourseError(DomainError):
+    pass
+
+
+class InvalidModuleError(DomainError):
+    pass
+
+
+class InvalidSectionError(DomainError):
+    pass
+
+
+class InvalidLectureError(DomainError):
+    pass
+
+
+class InvalidUserError(DomainError):
     pass
